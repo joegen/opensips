58b540c615573234a306c37a61d1d1ea2f25e211
Liviu Chircu <liviu@opensips.org>
2026-02-02 18:29:36 +0200

    mid_registrar: Fix the `tcp_persistent_flag` feature
    Make sure to NOT adjust the @e_max with -get_act_time(), similar to
    registrar codebase, to avoid setting MAX_INT on the TCP conn lifetime...

--- modules/mid_registrar/save.c
+++ modules/mid_registrar/save.c
@@ -1492,12 +1492,11 @@ update_usrloc:
 				LM_ERR("failed to parse contact <%.*s>\n",
 						ctmap->req_ct_uri.len, ctmap->req_ct_uri.s);
 			} else if ( is_tcp_based_proto(uri.proto) ) {
-				if (e_max) {
+				if (e_max)
 					LM_WARN("multiple TCP contacts on single REGISTER\n");
-					if (e_out>e_max) e_max = e_out;
-				} else {
-					e_max = e_out;
-				}
+
+				if (ctmap->expires > e_max)
+					e_max = ctmap->expires;
 			}
 		}
 	}
@@ -1526,7 +1525,7 @@ update_usrloc:
 	remove_expires_hf(rpl);
 
 	if ( tcp_check && e_max>0 ) {
-		e_max -= get_act_time();
+		LM_DBG("ensure TCP conn lifetime of at least %d sec\n", (e_max + 10));
 		trans_set_dst_attr( &req->rcv, DST_FCNTL_SET_LIFETIME,
 			(void*)(long)(e_max + 10) );
 	}
@@ -1748,12 +1747,11 @@ update_usrloc:
 				LM_ERR("failed to parse contact <%.*s>\n",
 				       ctmap->req_ct_uri.len, ctmap->req_ct_uri.s);
 			} else if ( is_tcp_based_proto(uri.proto) ) {
-				if (e_max) {
+				if (e_max)
 					LM_WARN("multiple TCP contacts on single REGISTER\n");
-					if (e_out>e_max) e_max = e_out;
-				} else {
-					e_max = e_out;
-				}
+
+				if (ctmap->expires > e_max)
+					e_max = ctmap->expires;
 			}
 		}
 	}
@@ -1777,7 +1775,7 @@ update_usrloc:
 	}
 
 	if ( tcp_check && e_max>0 ) {
-		e_max -= get_act_time();
+		LM_DBG("ensure TCP conn lifetime of at least %d sec\n", (e_max + 10));
 		trans_set_dst_attr( &req->rcv, DST_FCNTL_SET_LIFETIME,
 			(void*)(long)(e_max + 10) );
 	}
